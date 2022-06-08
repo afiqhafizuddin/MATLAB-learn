4462513c467fb174cd9fe2d61b8b07417d1db12e
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-06-08 22:33:41 +0800

    add two matlab files: using MATLAB as calculator

---
+++ calc.m
@@ -0,0 +1,70 @@
+% copy & paste the code
+
+1 + 1
+
+% spacing improves readability
+
+4 * 5 + 8
+
+% parentheses improves readability
+
+5*3/6^2
+
+(5 * 3) / (6^2)
+
+% order-of-operations
+
+3^2.3 + 4.1
+
+3^(2.3+4.1)
+
+% COLON OPERATOR
+
+% basic use
+
+1:5
+
+7:109
+
+-15:4
+
+% non-default skipping
+
+1:.5:5
+
+% count backwards
+
+10:-1:1
+
+% MATLAB stays within boundaries
+
+1:5:10
+
+
+% EXERCISE
+
+% 1
+
+((1:2:9)+5)/2
+
+((0:2:9)+5)/2
+
+% 2
+((9/3)+(4/2))^(( (2)+sqrt(9) ))
+
+(9/3 + 4/2)^(2 + sqrt(9))
+
+% Convert to -1 0 1
+2:2:6
+(2:2:6)/2 - 2
+
+% Convert -1 0 1 to 10 100 1000
+
+10.^ ((-1:1)+2)
+
+% convert 1:9 into [1 1 1 2 2 2 3 3 3]
+
+(1:9)/3
+
+
+ceil((1:9)/3)
